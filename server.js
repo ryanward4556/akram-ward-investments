dbb7aacc87baf4d15e71b69001a061a8929fafd3
Ryan Ward <ryanward4556@gmail.com>
2019-09-21 12:36:22 -0400

    added basic server file

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
-console.log("starter push");
-console.log("starter push2");
+// Dependencies
+// =============================================================
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = module.exports = express();
+var PORT = process.env.PORT || 3000;
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on http://localhost:" + PORT);
+});
