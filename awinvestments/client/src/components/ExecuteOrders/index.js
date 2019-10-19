10a3adc3288222eaf41e156c884ddf8b9a2f47d4
Martin Akram <martin.akram@gmail.com>
2019-10-19 11:49:17 -0400

    more alpaca documentation

--- awinvestments/client/src/components/ExecuteOrders/index.js
+++ awinvestments/client/src/components/ExecuteOrders/index.js
@@ -1,20 +1,76 @@
 import React, { Component } from "react";
 import API from "../../Utilities-ExecuteOrders/API";
+import { Container, Row, Col } from "../Grid";
+import Input from "../Input";
+import Button from "../Button";
 
 class ExecuteOrders extends Component {
-    handleOrderSubmit = () => {
-        API.submitOrder("1", "MSFT", "buy")
-        // .then(res => console.log(res))
-        // .catch(err => console.log(err));
-    };
+  state = {
+    quantity: "",
+    stock: "",
+    side: ""
+  }
 
-    render() {
-        return (
-            <div
-                makeOrder={this.handleOrderSubmit()}
-            ></div>
-        )
-    }
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+
+  handleOrderSubmit = event => {
+    event.preventDefault();
+    API.submitOrder(this.state.quantity, this.state.stock, this.state.side)
+  };
+
+  render() {
+    return (
+      <div class="row">
+        <div class="col-12">
+          <form>
+            <Container>
+              <Row>
+                <div className="col-3">
+                  <Input
+                    name="quantity"
+                    value={this.state.quantity}
+                    onChange={this.handleInputChange}
+                    placeholder="Enter Quantity"
+                  />
+                </div>
+                <div className="col-3">
+                  <Input
+                    name="stock"
+                    value={this.state.stock}
+                    onChange={this.handleInputChange}
+                    placeholder="Enter Stock Symbol"
+                  />
+                </div>
+                <div className="col-3">
+                  <Input
+                    name="side"
+                    value={this.state.side}
+                    onChange={this.handleInputChange}
+                    placeholder="Buy or Sell"
+                  />
+                </div>
+                <div className="col-3">
+                  <Button
+                    onClick={this.handleOrderSubmit}
+                    type="success"
+                    className="input-lg"
+                  >Search</Button>
+                </div>
+              </Row>
+            </Container>
+          </form>
+        </div>
+      </div>
+
+
+    )
+  }
 }
 
 export default ExecuteOrders;
